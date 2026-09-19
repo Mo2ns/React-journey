a21d2dfdb0329d49adc6f1271376bd8deb0ce258
Munis <monsalsharefeen@gmail.com>
2026-09-19 15:15:40 +0300

    adding main content structure

--- project-setup/src/Components/MainContent.jsx
+++ project-setup/src/Components/MainContent.jsx
@@ -1,7 +1,14 @@
 function MainContent() {
   return (
     <main>  
-      <h1>Main Content goes here</h1>
+      <h1>Main facts about React</h1>
+      <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
   )
 }
