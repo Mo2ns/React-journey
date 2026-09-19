d0576f0a3eccfecc26fd2ecfb99b6cf54d2cbec3
Munis <monsalsharefeen@gmail.com>
2026-09-19 14:19:17 +0300

    Adding navbar structure

--- project-setup/src/Components/Navbar.jsx
+++ project-setup/src/Components/Navbar.jsx
@@ -1,8 +1,23 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+import reactLogo from '../assets/react.svg'
+
 function Navbar() {
   return (
-    <nav>
-      <h1>Navbar goes here</h1>
-    </nav>
+    <header className="navbar">
+        <nav>
+            <img src={reactLogo} alt="React Logo" width="40" height="40" />
+            <h1>ReactFacts</h1>
+        </nav>
+    </header>
   )
 }
 
